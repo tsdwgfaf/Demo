af2e77f90162947a5df0a776248f282dfda65daf
Jacobi <1183710125@stu.hit.edu.cn>
2020-11-28 13:24:03 +0800

    “我的”界面 按钮功能

--- app/src/main/java/com/example/demo/ui/mine/MineItemView.java
+++ app/src/main/java/com/example/demo/ui/mine/MineItemView.java
@@ -15,10 +15,11 @@ import androidx.annotation.Nullable;
 
 import com.example.demo.R;
 
-public class MineItemView extends LinearLayout {
+public class MineItemView extends RelativeLayout {
     private ImageView imageView;
     private TextView textView;
     private RelativeLayout relativeLayout;
+    private RelativeLayout line;
     public MineItemView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(getContext()).inflate(R.layout.mine_item_view,this);
@@ -26,13 +27,16 @@ public class MineItemView extends LinearLayout {
         imageView = findViewById(R.id.mine_icon);
         textView = findViewById(R.id.mine_text);
         textView.setText(typedArray.getString(R.styleable.MineItemView_mine_text));
-        imageView.setBackgroundResource(typedArray.getResourceId(R.styleable.MineItemView_mine_icon, R.drawable.ic_baseline_settings_24));
+        imageView.setBackgroundResource(typedArray.getResourceId(R.styleable.MineItemView_mine_icon, R.drawable.settings_24));
         relativeLayout = findViewById(R.id.mine_root);
-        LinearLayout layout = findViewById(R.id.mine_item_view);
-        layout.setOnTouchListener(new View.OnTouchListener(){
+        line = findViewById(R.id.mine_line);
+        if (typedArray.getBoolean(R.styleable.MineItemView_mine_line, false)) {
+            line.setBackgroundColor(getResources().getColor(R.color.light_gray));
+        }
+        this.setOnTouchListener(new View.OnTouchListener(){
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()){
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN: {
                         relativeLayout.setBackgroundColor(getResources().getColor(R.color.gray));
                         break;
@@ -42,6 +46,9 @@ public class MineItemView extends LinearLayout {
                     }
                     case MotionEvent.ACTION_UP: {
                         relativeLayout.setBackgroundColor(getResources().getColor(R.color.white));
+                        if (textView.getText().toString().compareTo("设置") == 0) {
+
+                        }
                         break;
                     }
                     default:
