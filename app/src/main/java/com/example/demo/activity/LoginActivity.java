48b8b458721e89146560400ea29ab4e866322484
yusnow <tsdwgfaf@163.com>
2020-12-19 07:34:09 +0800

    添加聊天内容可复制功能

--- app/src/main/java/com/example/demo/activity/LoginActivity.java
+++ app/src/main/java/com/example/demo/activity/LoginActivity.java
@@ -6,14 +6,14 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.demo.R;
-
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        //setContentView(R.layout.activity_login);
     }
 
     public void jumpToMenu(View view) {
